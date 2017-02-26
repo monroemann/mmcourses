d7e1e330308e4d89d4a4ea64701dec466aa30068
Monroe Mann <findingsummer@aol.com>
2017-02-26 16:21:07 +0000

    Attempted enrollment authentication

--- app/controllers/enrollments_controller.rb
+++ app/controllers/enrollments_controller.rb
@@ -1,5 +1,6 @@
 class EnrollmentsController < ApplicationController
     before_action :authenticate_user!
+    before_action :require_authorized_for_current_course, only: [:show]
     
   def create
     current_user.enrollments.create(course: current_course)
@@ -12,5 +13,12 @@ class EnrollmentsController < ApplicationController
     @current_course ||= Course.find(params[:course_id])
   end
 
+  def require_authorized_for_current_course
+    if current_course.user != current_user && 
+        if !current_lesson.section.course.user.enrolled_in?(current_course)
+            redirect_to course_path(current_course), 
+            alert: 'You are not enrolled in this course'
+        end
+    end
 
 end
