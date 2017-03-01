3bc3f00fba8b122ad848ce766985f128047c5403
Monroe Mann <findingsummer@aol.com>
2017-03-01 00:15:11 +0000

    Working on authentication logic

--- app/controllers/lessons_controller.rb
+++ app/controllers/lessons_controller.rb
@@ -1,5 +1,6 @@
 class LessonsController < ApplicationController
     before_action :authenticate_user!
+    before_action :require_authorized_for_current_course, only: [:show]
 
     def show
     end
@@ -11,4 +12,15 @@ class LessonsController < ApplicationController
         @current_lesson ||= Lesson.find(params[:id])
       end
 
+    def current_course
+      @current_course ||= current_lesson.section.course
+    end
+
+    def require_authorized_for_current_course
+      if current_course.user != current_user && 
+        current_user.enrolled_in?(current_course)
+          redirect_to course_path(current_course), 
+            alert: 'You are not enrolled in this course'
+      end
+    end
 end
