fb930fe4215a0111a6cd7f825599600187b9bcc8
Monroe Mann <findingsummer@aol.com>
2017-03-03 17:44:03 +0000

    Making Stripe track payments

--- app/controllers/enrollments_controller.rb
+++ app/controllers/enrollments_controller.rb
@@ -3,7 +3,6 @@ class EnrollmentsController < ApplicationController
         
   def create
     current_user.enrollments.create(course: current_course)
-    redirect_to course_path(current_course)
   end
 
   private
@@ -11,5 +10,24 @@ class EnrollmentsController < ApplicationController
   def current_course
     @current_course ||= Course.find(params[:course_id])
   end
+
+    # Amount in cents
+    @amount = 500
+
+    customer = Stripe::Customer.create(
+      email: params[:stripeEmail],
+      source: params[:stripeToken]
+    )
+
+    charge = Stripe::Charge.create(
+      customer: customer.id,
+      amount: @amount,
+      description: 'Rails Stripe customer',
+      currency: 'usd'
+    )
+    redirect_to course_path(current_course)
+    rescue Stripe::CardError => e
+    flash[:error] = e.message
+    redirect_to root_path
 
 end
