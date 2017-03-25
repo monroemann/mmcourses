b5534dbebb2e13597bc177dd9f6758d0aa3f2985
Monroe Mann <findingsummer@aol.com>
2017-03-25 23:13:23 +0000

    Added an instructor dashboard

--- app/controllers/instructor/sections_controller.rb
+++ app/controllers/instructor/sections_controller.rb
@@ -43,12 +43,6 @@ class Instructor::SectionsController < ApplicationController
   end
 
   def require_authorized_for_current_section
-    if current_lesson.section.course.user != current_user
-      render text: 'Unauthorized', status: :unauthorized
-    end
-  end
-  
-  def require_authorized_for_current_section
     if current_section.course.user != current_user
      render text: 'Unauthorized', status: :unauthorized
     end
